dc70916bbf91a3e602afb4876850cba316f28c35
DeemProductTeam <67124316+DeemProductTeam@users.noreply.github.com>
2020-07-28 09:27:45 -0400

    Add files via upload

---
+++ studio/schemas/documents/applepost.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'applepost',
+  type: 'document',
+  title: 'Apple Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description'
+    }
+  ]
+}
